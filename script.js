111e24f5909e008642e377d26445a411bd68fa8f
Diwakar Kumar <51357882+Diwakarkumar3096@users.noreply.github.com>
2022-10-21 03:19:24 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let String = " ";
+let buttons = document.querySelectorAll(".btn");
+Array.from(buttons).forEach((button) => {
+  button.addEventListener('click', (e) => {
+    if (e.target.innerHTML == '=') {
+      String = eval(String);
+      document.querySelector('input').value = String;
+    }
+    else if (e.target.innerHTML == 'AC') {
+      String = " ";
+      document.querySelector('input').value = String;
+    }
+    else {
+      console.log(e.target);
+      String = String + e.target.innerHTML;
+      document.querySelector('input').value = String;
+    }
+
+  });
+
+})
